baca097080826de88798671f9e695ce294744181
jeremyfrog20 <tylerdayton@comcast.net>
2020-01-22 03:50:29 -0800

    Both assignment and Unit Testing are done

--- Assignment2/Rectangle.cs
+++ Assignment2/Rectangle.cs
@@ -5,78 +5,120 @@ using System.Text;
 
 namespace Rectangle
 {
-    class Rectangle
+    public class Rectangle
     {
         private Point upperLeftVertex, lowerRightVertex;
 
         private Point newUpperLeft(in Point p1, in Point p2)
         {
+            double lowX, highY;
 
+            //Comparing x
+            if (p1.getX() < p2.getX())
+                lowX = p1.getX();
+            else
+                lowX = p2.getX();
+
+            //Comparing y
+            if (p1.getY() > p2.getY())
+                highY = p1.getY();
+            else
+                highY = p2.getY();
+
+            return new Point(lowX, highY);
         }
-        
-        private Point newUpperLeft(in Point p1, in double width, in double height)
+        private Point newUpperLeft(in Point p1, in double height)
         {
-
+            return new Point(p1.getX(), (p1.getY() + height));
         }
         private Point newLowerRight(in Point p1, in Point p2)
         {
-            
+            double highX, lowY;
+
+            //Comparing x
+            if (p1.getX() > p2.getX())
+                highX = p1.getX();
+            else
+                highX = p2.getX();
+
+            //Comparing y
+            if (p1.getY() < p2.getY())
+                lowY = p1.getY();
+            else
+                lowY = p2.getY();
+
+            return new Point(highX, lowY);
         }
 
-        private Point newLowerRight(in Point p1, in double width, in double height)
+        private Point newLowerRight(in Point p1, in double width)
         {
-
+            return new Point((p1.getX() + width), p1.getY());
         }
 
         //Public
         public Rectangle(in Point p1, in Point p2)
         {
-            
+            upperLeftVertex = newUpperLeft(p1, p2);
+            lowerRightVertex = newLowerRight(p1, p2);
         }
 
         public Rectangle(in Point p1, in double width, in double height)
         {
-
+            upperLeftVertex = newUpperLeft(p1, height);
+            lowerRightVertex = newLowerRight(p1, width);
         }
 
         public void translate (in double xAmount, in double yAmount)
         {
-
+            upperLeftVertex.setX(upperLeftVertex.getX() + xAmount);
+            lowerRightVertex.setX(lowerRightVertex.getX() + xAmount);
+            upperLeftVertex.setY(upperLeftVertex.getY() + yAmount);
+            lowerRightVertex.setY(lowerRightVertex.getY() + yAmount);
         }
 
         public double getWidth()
         {
-            
+            return lowerRightVertex.getX() - upperLeftVertex.getX();
         }
 
         public double getHeight()
         {
-
+            return upperLeftVertex.getY() - lowerRightVertex.getY();
         }
 
         public Point getUpperLeftVertex()
         {
-
+            return upperLeftVertex;
         }
 
         public Point getCenter()
         {
-
+            return new Point((getWidth() / 2), (getHeight() / 2));
         }
 
-        double getArea()
+        public double getArea()
         {
-
+            return getWidth() * getHeight();
         }
 
-        double getPerimeter()
+        public double getPerimeter()
         {
-
+            return (2 * getWidth()) + (2 * getHeight());
         }
 
-        bool contains(in Point p)
+        public bool contains(in Point p)
         {
+            //Within X bounds
+            if ((upperLeftVertex.getX() <= p.getX()) && (lowerRightVertex.getX() >= p.getX()))
 
+            {
+                //Within Y bounds
+                if ((upperLeftVertex.getY() >= p.getY()) && (lowerRightVertex.getY() <= p.getY()))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
 
--- Rectangle Tester/RectangleTester.cs
+++ Rectangle Tester/RectangleTester.cs
@@ -1,15 +1,152 @@
 ﻿using System;
+using Rectangle;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rect = Rectangle.Rectangle;
+using Point = Rectangle.Point;
 
-namespace Rectangle_Tester
+namespace Rect_Tester
 {
+    
     [TestClass]
-    public class RectangleTester
+    public class RectTester
     {
+        
         [TestMethod]
         public void Constructor2Args()
         {
+            //Does is initalize the correct bounds of the Rect?
+            //(P1 x0, P1 y10) test
+            Rect test1= new Rect(new Point(0, 10), new Point(5, 5));
+            Assert.AreEqual(test1.getUpperLeftVertex().getX(), 0);
+            Assert.AreEqual(test1.getUpperLeftVertex().getY(), 10);
+            //Width and Height Assert.AreEqual for lowerRight boundry
+            Assert.AreEqual(test1.getWidth(), 5);
+            Assert.AreEqual(test1.getHeight(), 5);
+            
+            //(P1 x0, P2 y5) test
+            Rect test2 = new Rect(new Point(0, 0), new Point(5, 5));
+            Assert.AreEqual(test2.getUpperLeftVertex().getX(), 0);
+            Assert.AreEqual(test2.getUpperLeftVertex().getY(), 5);
+            //Width and Height Assert.AreEqual for lowerRight boundry
+            Assert.AreEqual(test2.getWidth(), 5);
+            Assert.AreEqual(test2.getHeight(), 5);
 
+            //(P2 x5, P1 y10) test
+            Rect test3 = new Rect(new Point(10, 10), new Point(5, 5));
+            Assert.AreEqual(test3.getUpperLeftVertex().getX(), 5);
+            Assert.AreEqual(test3.getUpperLeftVertex().getY(), 10);
+            //Width and Height Assert.AreEqual for lowerRight boundry
+            Assert.AreEqual(test3.getWidth(), 5);
+            Assert.AreEqual(test3.getHeight(), 5);
+
+            //(P2 x5, P2 y5) test
+            Rect test4 = new Rect(new Point(10, 0), new Point(5, 5));
+            Assert.AreEqual(test4.getUpperLeftVertex().getX(), 5);
+            Assert.AreEqual(test4.getUpperLeftVertex().getY(), 5);
+            //Width and Height Assert.AreEqual for lowerRight boundry
+            Assert.AreEqual(test4.getWidth(), 5);
+            Assert.AreEqual(test4.getHeight(), 5);
+
+        }
+
+        [TestMethod]
+        public void Constructor3Args()
+        {
+            //Less testing seems to be needed for this,
+            //as the width and height cannot be smaller than the initial point
+
+            //(P1 x0, ) test
+            //Same as rec.upperLeft = (0, 5);
+            //rec.lowerRight = (5, 0);
+            Rect test1 = new Rect(new Point(0, 0), 5, 3);
+            //UpperLeft Assert.AreEqual
+            Assert.AreEqual(test1.getUpperLeftVertex().getX(), 0);
+            Assert.AreEqual(test1.getUpperLeftVertex().getY(), 3);
+            //Width and Height Assert.AreEqual for lowerRight
+            Assert.AreEqual(test1.getWidth(), 5);
+            Assert.AreEqual(test1.getHeight(), 3);
+        }
+        
+        [TestMethod]
+        public void Translate()
+        {
+            Rect rec = new Rect(new Point(), 3, 5);
+            rec.translate(5, 5);
+
+            //Did it move upperLeft?
+            Assert.AreEqual(rec.getUpperLeftVertex().getX(), 5);
+            Assert.AreEqual(rec.getUpperLeftVertex().getY(), 10);
+
+            //Did it move lowerRight?
+            Assert.AreEqual(rec.getWidth(), 3);
+            Assert.AreEqual(rec.getHeight(), 5);
+
+        }
+
+        [TestMethod]
+        public void GetWidth()
+        {
+            //Find the width from a two point constructor?
+            Assert.AreEqual(3, new Rect(new Point(), new Point(3, 5)).getWidth());
+            //Finds the width from 3 arg constructor?
+            Assert.AreEqual(3, new Rect(new Point(), 3, 5).getWidth());
+            
+        }
+
+        [TestMethod]
+        public void GetHeight()
+        {
+            //Find the height from a two point constructor?
+            Assert.AreEqual(new Rect(new Point(), new Point(3, 5)).getHeight(), 5);
+            //Finds the height from 3 arg constructor?
+            Assert.AreEqual(new Rect(new Point(), 3, 5).getHeight(), 5);
+        }
+
+        [TestMethod]
+        public void GetCenter()
+        {
+            Rect rec = new Rect(new Point(), new Point(3, 5));
+            Point center = rec.getCenter();
+
+            //Correct center?
+            Assert.AreEqual(center.getX(), 1.5);
+            Assert.AreEqual(center.getY(), 2.5);
+        }
+
+        [TestMethod]
+        public void GetPerimeter()
+        {
+            //Adds perimeter correctly?
+            Assert.AreEqual(new Rect(new Point(), new Point(3, 5)).getPerimeter(), 16);
+        }
+
+        [TestMethod]
+        public void GetArea()
+        {
+            //Multiplies area correctly?
+            Assert.AreEqual(new Rect(new Point(), new Point(3, 5)).getArea(), 15);
+        }
+
+        [TestMethod]
+        public void GetUpperLeftVertex()
+        {
+            //p1 will be upperLeft
+            Point p1 = new Point(0, 10);
+            Point p2 = new Point(5, 5);
+            Rect rec = new Rect(p1, p2);
+            //Is p1 same as upperLeft?
+            Assert.AreEqual(rec.getUpperLeftVertex().getX(), p1.getX());
+            Assert.AreEqual(rec.getUpperLeftVertex().getY(), p1.getY());
+        }
+
+        [TestMethod]
+        public void Contains()
+        {
+            Rect rec = new Rect(new Point(), new Point(3, 5));
+            //Assert.AreEquals point within the Rect
+            Assert.AreEqual(rec.contains(new Point(1, 1)), true);
+            //Assert.AreEquals point outside of the Rect
+            Assert.AreEqual(rec.contains(new Point(10, 10)), false);
         }
     }
 }
