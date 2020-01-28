ce91d940c58d49936bdfc967e58a42ccd81ff31b
jeremyfrog20 <tylerdayton@comcast.net>
2020-01-28 06:28:37 -0800

    Weird locking error, but close to done

--- Assignment3/Page.cs
+++ Assignment3/Page.cs
@@ -6,6 +6,59 @@ namespace Assignment3
 {
     class Page
     {
+        public Page()
+        {
+            rand = new Random();
+            url = "";
+            visits = 0;
+            usedLinks = 0;
+            links = new Page[3];
+        }
 
+        public Page(in string url)
+        {
+            rand = new Random();
+            this.url = url;
+            visits = 0;
+            usedLinks = 0;
+            links = new Page[3];
+        }
+
+        public string getURL()
+        {
+            return url;
+        }
+
+        public int getNumLinks()
+        {
+            return usedLinks;
+        }
+
+        public ref Page getRandomLink()
+        {
+            return ref links[rand.Next(0, usedLinks)];
+        }
+
+        public void addLink(ref Page other)
+        {
+            links[usedLinks] = other;
+            usedLinks++;
+        }
+
+        public void visit()
+        {
+            visits++;
+        }
+
+        public int getNumVisits()
+        {
+            return visits;
+        }
+
+        private Random rand;
+        private string url;
+        private int visits, usedLinks;
+        //This isn't a pointer, but c# does this different.
+        private Page[] links;
     }
 }
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -2,40 +2,81 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
 namespace Assignment3
 {
-    //Allow for some managed types to be used as pointers
-    unsafe class VarPointer
-    {
-        private Type objType;
-        private char*[] stringAddresses;
-
-        internal Type ObjType { get => objType; set => objType = value; }
-
-        public enum Type
-        {
-            String
-        }
-        public VarPointer(string input)
-        {
-            objType = Type.String;
-            stringAddresses = new char*[input.Length];
-
-        }
-    }
     class Program
     {
-        static void Main(string[] args)
+        static public void initLinks(ref Page page1, ref Page page2, ref Page page3, ref Page page4)
         {
-            unsafe
-            {
-                string value = "asdf";
-                VarPointer pointer = new VarPointer(value);
+            page1.addLink(ref page2);
+            page1.addLink(ref page3);
 
-                Console.WriteLine();
+            page2.addLink(ref page3);
+
+            page3.addLink(ref page1);
+
+            page4.addLink(ref page1);
+            page4.addLink(ref page3);
+        }
+
+        static ref Page findNewPage(ref Page currentPage, ref Page page1, ref Page page2, ref Page page3, ref Page page4)
+        {
+            int randNum = rand.Next(1, 100);
+
+            if ((currentPage.getNumLinks() == 0) || (randNum <= 15))
+            {
+                int pageNum = rand.Next(0, 3);
+
+                switch(pageNum)
+                {
+                    case 0: return ref page1;
+                    case 1: return ref page2;
+                    case 2: return ref page3;
+                    case 3: return ref page4;
+                    default: throw new IndexOutOfRangeException();
+                }
             }
 
-            Console.WriteLine("Hello World!");
+            return ref currentPage.getRandomLink();
+        }
+        const int LIMIT = 10000;
+        static public void simulate(ref Page page1, ref Page page2, ref Page page3, ref Page page4)
+        {
+            //does this copy?
+            Page currentPage = page1;
+
+            for (int i = 0; i < LIMIT; i++)
+            {
+                currentPage = findNewPage(ref currentPage, ref page1, ref page2, ref page3, ref page4);
+                currentPage.visit();
+            }
+        }
+
+        static void printResult(ref Page page1, ref Page page2, ref Page page3, ref Page page4)
+        {
+            Console.WriteLine($"{page1.getURL()} got {((double)page1.getNumVisits() / LIMIT) * 100}% of the visits\n");
+            Console.WriteLine($"{page2.getURL()} got {((double)page2.getNumVisits() / LIMIT) * 100}% of the visits\n");
+            Console.WriteLine($"{page3.getURL()} got {((double)page3.getNumVisits() / LIMIT) * 100}% of the visits\n");
+            Console.WriteLine($"{page4.getURL()} got {((double)page4.getNumVisits() / LIMIT) * 100}% of the visits\n");
+
+
+        }
+
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            Page page1 = new Page("http://page1");
+            Page page2 = new Page("http://page2");
+            Page page3 = new Page("http://page3");
+            Page page4 = new Page("http://page4");
+
+            initLinks(ref page1, ref page2, ref page3, ref page4);
+
+            simulate(ref page1, ref page2, ref page3, ref page4);
+
+            printResult(ref page1, ref page2, ref page3, ref page4);
+
         }
     }
 }
